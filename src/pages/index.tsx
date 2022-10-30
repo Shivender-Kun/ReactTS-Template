b513d438a38651254459f7faac6af2ffc74ab891
Shivender-Kun <shivenderkumar761@gmail.com>
2022-10-30 16:23:27 +0530

    eslint, prettier added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,13 @@
-function Pages() {
-  return <div>Pagess</div>;
-}
+import { useState } from "react";
+
+const Pages = () => {
+  const [count, setcount] = useState("0");
+
+  return (
+    <div>
+      Pages <button onClick={() => setcount((c) => c + 1)}>{count}</button>
+    </div>
+  );
+};
 
 export default Pages;
